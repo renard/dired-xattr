70419f20eaed66b5ee0465feba8370c7d44e37ff
Sébastien Gross <sebchezwamorg>
2013-01-10 15:57:58 +0100

    Fix typo.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- dired-xattr.el
+++ dired-xattr.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2013-01-09
-;; Last changed: 2013-01-10 15:56:29
+;; Last changed: 2013-01-10 15:57:44
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -109,7 +109,7 @@
 (defconst dired-xattr-labels
   '(:1 "Grey"
     :2 "Green"
-    :3 "Pupple"
+    :3 "Purple"
     :4 "Blue"
     :5 "Yellow"
     :6 "Red"
