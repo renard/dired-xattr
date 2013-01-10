4fdc84d81a64d253e2af99e6335e57c230b6328c
Sébastien Gross <sebchezwamorg>
2013-01-10 12:20:40 +0100

    Add ucs-normalize to requirement.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- dired-xattr.el
+++ dired-xattr.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2013-01-09
-;; Last changed: 2013-01-10 12:17:46
+;; Last changed: 2013-01-10 12:20:22
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -23,7 +23,8 @@
 (eval-when-compile
   (require 'cl)
   (require 'tramp)
-  (require 'dired))
+  (require 'dired)
+  (require 'ucs-normalize))
 
 (defgroup dired-xattr nil
   "Manipulate MacOSX extanded attributes in Dired."
